461498ee7554d3829c975c85eda396537de66931
Reman Swati <yemshams@gmail.com>
2023-09-07 15:18:25 +0500

    Transactions section completed, changes in blocks

--- src/components/Blocks.js
+++ src/components/Blocks.js
@@ -1,48 +1,42 @@
 import React from 'react'
 
 const Blocks = () => {
-  var elements = [1,2,3,4,5,6,7];
+  const blockSections = [];
+  
+  for (let i = 0; i < 7; i++) {
+    blockSections.push(
+    <div className="">
+      <div>
+        <div className="text-blue text-sm">
+          <div className="text-black inline-block">Block</div>
+          <a href='' className="ml-1">9614567</a>
+          <span className="text-black opacity-50"> 45 sec ago</span>
+        </div>
+        <div className="text-black text-sm">Fee Recipient 
+          <a href='' className="text-blue ml-1">0xc6e245...A1E4Cb97</a>
+          <div className="text-blue text-sm">
+            <a href=''>39 txns</a>
+            <span className="text-black opacity-50 ml-1">12 secs</span>
+            <span className="bg-white text-black border-[1px] text-[12px] border-black px-2 ml-1 rounded-md border-opacity-50">0.00146 Eth</span>
+          </div>
+        </div>
+      </div>
+      {i != 6 ? <div className='h-[1px] bg-black bg-opacity-25 my-3'/> : null}
+    </div>);
+  }
+
   return (
-    <div className="h-[683px] bg-light_grey mx-4 mt-[-65px] flex flex-col justify-between rounded-lg border-[1px] border-black border-opacity-25">
+    <div className="bg-light_grey mx-4 mt-[-65px] flex flex-col justify-between rounded-lg border-[1px] border-black border-opacity-25">
       <div className="h-9 rounded-lg">
         <h2 className='p-2 text-lg'>Latest Blocks</h2>
         <div className='h-[1px] bg-black bg-opacity-25' />
       </div>
-      <div className="px-2">
-        {
-          elements.map((index)=>{
-            if(index != 7){return(
-              <div className="py-2">
-                  <div className="text-blue text-sm">9614567 <span className="text-black opacity-50">45 sec ago</span></div>
-                  <div className="text-black text-sm">Fee Recipient <span className="text-blue">0xc6e245...A1E4Cb97</span></div>
-                  <div className="text-blue text-sm">39 txns
-                    <span className="text-black opacity-50"> 12 secs</span> 
-                    <span className="bg-white text-black border-[1px] text-[12px] border-black px-2 ml-1 rounded-md border-opacity-50">0.00146 Eth</span>
-                  </div>
-                <div className='h-[1px] bg-black bg-opacity-25 mt-3'/>
-              </div>
-            )}else{
-              return(
-                <div className="py-2                                                                                       ">
-                  <div className="text-blue text-sm">9614567 <span className="text-black opacity-50">45 sec ago</span></div>
-                  <div className="text-black text-sm">Fee Recipient <span className="text-blue">0xc6e245...A1E4Cb97</span></div>
-                  <div className="text-blue text-sm">39 txns
-                    <span className="text-black opacity-50"> 12 secs</span> 
-                    <span className="bg-white text-black border-[1px] border-black px-2 ml-1 rounded-md border-opacity-50">0.00146 Eth</span>
-                  </div>
-                </div>  
-              )
-            }
-          })
-        }
-            
-          
-        
-      </div>
+      
+      <div className="mx-2 my-4">{blockSections}</div>
       
       <div className="h-9 rounded-lg bg-grey">
         <div className='h-[1px] bg-black bg-opacity-25' />
-        <h3 className="p-2 opacity-50 text-sm flex justify-center">VIEW ALL BLOCKS</h3>
+        <a href='' className="p-2 opacity-50 text-sm flex justify-center">VIEW ALL BLOCKS</a>
         
       </div>
     </div>
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,9 +1,44 @@
 import React from 'react'
 
 const Transactions = () => {
+  const blockSections = [];
+  
+  for (let i = 0; i < 7; i++) {
+    blockSections.push(
+    <div className="">
+      <div>
+        <div className="text-blue text-sm">
+        <div className="text-black inline-block">TX#</div>
+          <a href='' className="ml-1">0x04080540ad70...</a>
+          <span className="text-black opacity-50 ml-1">45 sec ago</span>
+        </div>
+        <div className="text-black text-sm">From 
+          <a href='' className="text-blue ml-1">0xc6e245...A1E4Cb97</a>
+          <div className="text-blue text-sm">
+            <span href='' className="text-black">To</span>
+            <a href='' className='ml-1'>0xc6e245...A1E4Cb97</a>
+            <span className="bg-white text-black border-[1px] text-[12px] border-black px-2 ml-1 rounded-md border-opacity-50">0.00146 Eth</span>
+          </div>
+        </div>
+      </div>
+      {i != 6 ? <div className='h-[1px] bg-black bg-opacity-25 my-3'/> : null}
+    </div>);
+  }
+
   return (
-    <div>
-      Hello world in Transactions
+    <div className="bg-light_grey mx-4 mt-6 flex flex-col justify-between rounded-lg border-[1px] border-black border-opacity-25">
+      <div className="h-9 rounded-lg">
+        <h2 className='p-2 text-lg'>Latest Transactions</h2>
+        <div className='h-[1px] bg-black bg-opacity-25' />
+      </div>
+      
+      <div className="mx-2 my-4">{blockSections}</div>
+      
+      <div className="h-9 rounded-lg bg-grey">
+        <div className='h-[1px] bg-black bg-opacity-25' />
+        <a href='' className="p-2 opacity-50 text-sm flex justify-center">VIEW ALL TRANSACTIONS</a>
+        
+      </div>
     </div>
   )
 }
